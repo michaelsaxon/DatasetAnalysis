1e8f0f409cd8edb1fa589d4ab12bd1c8037100f9
Michael Saxon <xmichaelsaxon@gmail.com>
2022-03-02 14:48:32 -0800

    debug tests

--- training/dataset_to_clusters.py
+++ training/dataset_to_clusters.py
@@ -101,6 +101,8 @@ def label_lists_to_arrays(label_lists):
     for i, key in enumerate(label_lists.keys()):
         X_list += label_lists[key]
         labels += [i] * len(label_lists[key])
+    print(X_list[0].shape)
+    print(X_list[1].shape)
     return np.concatenate(X_list, axis=0), np.array(labels)
 
 # this is a bundle of the two prev functions to interface with auto caching
