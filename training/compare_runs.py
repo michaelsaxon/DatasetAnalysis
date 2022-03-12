8b5c3470faf1f6d498b7a337dfe1c59ea2179aca
Michael Saxon <xmichaelsaxon@gmail.com>
2022-03-11 17:28:39 -0800

    added importance mapping

--- training/compare_runs.py
+++ training/compare_runs.py
@@ -1,5 +1,5 @@
 """
-CUDA_VISIBLE_DEVICES=0 python compare_runs.py --dataset A3
+CUDA_VISIBLE_DEVICES=1 python compare_runs.py --dataset A1
 """
 
 from train_classifier import *
@@ -13,6 +13,17 @@ def collect_posteriors(nli_dataset, ltmodel):
         batch_posts = ltmodel(input_ids = batch['input_ids'], attention_mask=batch['attention_mask'])
         yield batch_posts.logits, batch["labels"]
 
+def collect_importance_maps_and_posteriors(nli_dataset, ltmodel):
+    for batch in tqdm(nli_dataset.test_dataloader()):
+        cuda_dict(batch)
+        batch_posts = ltmodel(input_ids = batch['input_ids'], attention_mask=batch['attention_mask'])
+        local_grad = torch.autograd.grad(batch_posts.logits,
+            batch_posts.hidden_states[0], retain_graph = True,
+            grad_outputs = torch.ones_like(batch_posts.logits))[0]
+        local_importance_maps = torch.norm(local_grad, dim=2) / torch.sum(torch.norm(result, dim=2))
+        return local_importance_maps, batch_posts.logits, batch["labels"]
+
+
 def get_numpy_preds(nli_data, ltmodel):
     decisions_list = []
     labels_list = []
@@ -25,6 +36,26 @@ def get_numpy_preds(nli_data, ltmodel):
     decisions_list = np.concatenate(decisions_list)
     labels_list = np.concatenate(labels_list)
     return decisions_list, labels_list
+
+
+def get_numpy_preds_imp_maps(nli_data, ltmodel):
+    imp_maps_list = []
+    decisions_list = []
+    labels_list = []
+    for b_impmaps, b_posts, b_labs in collect_importance_maps_and_posteriors(nli_data, ltmodel):
+        print(batch_decisions.shape)
+        print(batch_impmaps.shape)
+        batch_impmaps = b_impmaps.cpu().detach().numpy()
+        imp_maps_list.append(batch_impmaps)
+        batch_decisions = torch.max(b_posts, -1).indices
+        batch_decisions = batch_decisions.cpu().detach().numpy()
+        batch_labs = b_labs.cpu().detach().numpy()
+        decisions_list.append(batch_decisions)
+        labels_list.append(batch_labs)
+    imp_maps_list = np.concatenate(imp_maps_list)
+    decisions_list = np.concatenate(decisions_list)
+    labels_list = np.concatenate(labels_list)
+    return decisions_list, labels_list, imp_maps_list
 
 
 @click.command()
@@ -57,7 +88,7 @@ def main(n_gpus, dataset, batch_size):
 
     print("Running model 1...")
 
-    dec_1, labs_1 = get_numpy_preds(nli_data_1, ltmodel)
+    dec_1, labs_1, maps_1 = get_numpy_preds_imp_maps(nli_data_1, ltmodel)
 
     ltmodel.load_state_dict(ckpt_2["state_dict"])
     model.cuda()
@@ -65,7 +96,7 @@ def main(n_gpus, dataset, batch_size):
 
     print("Running model 2...")
 
-    dec_2, labs_2 = get_numpy_preds(nli_data_2, ltmodel)
+    dec_2, labs_2, maps_2 = get_numpy_preds_imp_maps(nli_data_2, ltmodel)
 
     labs_1 = labs_1.squeeze()
     labs_2 = labs_2.squeeze()
