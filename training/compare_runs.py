425a557f01c013cac12a24365a691ebd8554d540
Michael Saxon <xmichaelsaxon@gmail.com>
2022-03-17 17:47:05 -0700

    really dumb bug lmao

--- training/compare_runs.py
+++ training/compare_runs.py
@@ -61,7 +61,7 @@ def get_numpy_preds_imp_maps(nli_data, ltmodel, pad_right):
             new_imp_maps = np.zeros_like(batch_impmaps)
             for i in range(new_imp_maps.shape[0]):
                 new_imp_maps[i,-lengths[i]:] += batch_impmaps[i,:lengths[i]]
-                batch_impmaps = new_imp_maps
+            batch_impmaps = new_imp_maps
         print(batch_impmaps)
         imp_maps_list.append(batch_impmaps)
         batch_decisions = torch.max(b_posts, -1).indices
