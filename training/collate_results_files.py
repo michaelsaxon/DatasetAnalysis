fcab5395ee8bb0ae32f4a69c16b920fd16110105
Michael Saxon <xmichaelsaxon@gmail.com>
2023-02-09 15:39:05 -0800

    collating result files

---
+++ training/collate_results_files.py
@@ -0,0 +1,56 @@
+import click
+from sklearn import cluster
+
+from train_classifier import *
+from manage_settings import get_write_settings, read_models_csv, lazymkdir
+from transformers import RobertaTokenizer, RobertaForSequenceClassification, BertTokenizer, BertForSequenceClassification
+
+from collections import defaultdict
+
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+import pickle
+
+import numpy as np
+
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+
+def single_set(fname):
+    lines = open(fname,"r").readlines()
+    return lines[1].split(",")[0:2]
+
+
+def collate(dataset, ld = True, part = 's2'):
+    outs = []
+    for k in [10, 25]:
+        for pc in [0, 50, 100]:
+            set = single_set(f"{ld * 'ld-'}{dataset}-test/results-{part}-pc{pc}-k{k}.csv")
+            outs += set
+    print(f"{dataset} & {part} & {' & '.join(outs)}")
+    outs = []
+    for k in [50, 100]:
+        for pc in [0, 50, 100]:
+            set = single_set(f"{ld * 'ld-'}{dataset}-test/results-{part}-pc{pc}-k{k}.csv")
+            outs += set
+    print(f"{dataset} & {part} & {' & '.join(outs)}")
+
+
+# ld s2only
+folders = 'A1 A2 A3 AA CF MB MdbA MU SdbA SICK S X'.split(" ")
+for dataset in folders:
+    collate(dataset, part='s1')
+
+
+# ld s1only
+folders = 'CF F MdbA SdbA'.split(" ")
+for dataset in folders:
+    collate(dataset)
+
+# s2only
+folders = ['OC']
+for dataset in folders:
+    collate(dataset, ld=False)
