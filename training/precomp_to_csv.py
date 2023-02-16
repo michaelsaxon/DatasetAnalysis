667906c5e2f6696a03441a9159da7bb4c9a05fe6
Michael Saxon <xmichaelsaxon@gmail.com>
2023-02-16 01:27:39 -0800

    ughghghghhh the embs ordering had to write code to reverse this

--- training/precomp_to_csv.py
+++ training/precomp_to_csv.py
@@ -86,7 +86,7 @@ def main(dataset, s1only, s2only, n_clusters, n_components, lastdense):
 
     _, s2, label, _ = zip(*load_nli_data(dir_settings["dataset_dir"] + "/", dataset, "test"))
     sentences_correct_order = replicate_label_group_shuffle_from_strings(s2, label)
-    print(len(sentences))
+    print(len(sentences_correct_order))
 
     with open(f"{intermed_comp_dir}/cluster_{n_clusters}_l2.csv","w") as f:
         f.writelines(["clid,peco_l2\n"] + [f"{i},{clusters_L2[i]}\n" for i in range(clusters_L2.shape[0])])
